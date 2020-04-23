66a7c6b460c2c27df6e72270af32f8e435fea505
KarenKuan <18055889@myrp.edu.sg>
2020-04-23 19:21:55 +0800

    Demo Activities

---
+++ app/src/main/java/sg/edu/rp/c346/demoactivities/DemoActivities2.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.demoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DemoActivities2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_activities2);
+    }
+}
